34294f06b3518c182c473b5a059dcaf3192db4c2
Ruben Krueger <34781887+Ruben-Krueger@users.noreply.github.com>
2025-06-20 12:48:16 -0700

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Footer from './Footer';
 
 export default function Home() {
@@ -6,13 +5,14 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <h1 className="font-bold text-xl">
-          I'm Ruben Krueger, a software engineer with a passion for building
-          products that help people live better lives.
+          I&apos;m Ruben Krueger, a software engineer with a passion for
+          building products that help people live better lives.
         </h1>
         <p className="max-w-prose text-center">
-          Previously: Stanford Computer Science (B.S. '21), Founding Engineer at
-          Comet Healthcare (YC S21), Research Data Analyst at the Brain
-          Stimulation Laboratory, and Software Engineer at Sprinter Health.
+          Previously: Stanford Computer Science (B.S. &apos;21), Founding
+          Engineer at Comet Healthcare (YC S21), Research Data Analyst at the
+          Brain Stimulation Laboratory, and Software Engineer at Sprinter
+          Health.
         </p>
       </main>
       <Footer />
